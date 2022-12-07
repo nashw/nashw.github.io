7f712627b97ddf0fdb5aaefeb1aee3361deff716
nashw <wesleyrnash@gmail.com>
2022-12-07 04:48:09 +0000

    expanded to all steps

--- script.js
+++ script.js
@@ -12,3 +12,48 @@ function fullPraline(){
     document.getElementById("details-praline").setAttribute("hidden", "")
     document.getElementById("full-praline").removeAttribute("hidden")
 }
+
+function hintChoux(){
+    document.getElementById("hint-choux-button").setAttribute("hidden", "")
+    document.getElementById("hint-choux").removeAttribute("hidden")
+}
+
+function detailsChoux(){
+    document.getElementById("hint-choux").setAttribute("hidden", "")
+    document.getElementById("details-choux").removeAttribute("hidden")
+}
+
+function fullChoux(){
+    document.getElementById("details-choux").setAttribute("hidden", "")
+    document.getElementById("full-choux").removeAttribute("hidden")
+}
+
+function hintCreme(){
+    document.getElementById("hint-creme-button").setAttribute("hidden", "")
+    document.getElementById("hint-creme").removeAttribute("hidden")
+}
+
+function detailsCreme(){
+    document.getElementById("hint-creme").setAttribute("hidden", "")
+    document.getElementById("details-creme").removeAttribute("hidden")
+}
+
+function fullCreme(){
+    document.getElementById("details-creme").setAttribute("hidden", "")
+    document.getElementById("full-creme").removeAttribute("hidden")
+}
+
+function hintAssemble(){
+    document.getElementById("hint-assemble-button").setAttribute("hidden", "")
+    document.getElementById("hint-assemble").removeAttribute("hidden")
+}
+
+function detailsAssemble(){
+    document.getElementById("hint-assemble").setAttribute("hidden", "")
+    document.getElementById("details-assemble").removeAttribute("hidden")
+}
+
+function fullAssemble(){
+    document.getElementById("details-assemble").setAttribute("hidden", "")
+    document.getElementById("full-assemble").removeAttribute("hidden")
+}
