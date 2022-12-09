f196d465000fb09c46eaf7977c997ce2dbe28120
nashw <wesleyrnash@gmail.com>
2022-12-09 03:39:04 +0000

    Changed to bossche bollen, added instructions for choux, cream, placeholders for rest

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function hintPraline(){
-    document.getElementById("hint-praline-button").setAttribute("hidden", "")
-    document.getElementById("hint-praline").removeAttribute("hidden")
-}
-
-function detailsPraline(){
-    document.getElementById("hint-praline").setAttribute("hidden", "")
-    document.getElementById("details-praline").removeAttribute("hidden")
-}
-
-function fullPraline(){
-    document.getElementById("details-praline").setAttribute("hidden", "")
-    document.getElementById("full-praline").removeAttribute("hidden")
-}
-
 function hintChoux(){
-    document.getElementById("hint-choux-button").setAttribute("hidden", "")
+    document.getElementById("hid-choux").setAttribute("hidden", "")
     document.getElementById("hint-choux").removeAttribute("hidden")
 }
 
@@ -28,8 +13,40 @@ function fullChoux(){
     document.getElementById("full-choux").removeAttribute("hidden")
 }
 
+function hideChoux(){
+    document.getElementById("full-choux").setAttribute("hidden", "")
+    document.getElementById("hid-choux").removeAttribute("hidden")
+}
+
+function ingItmChoux(){
+    document.getElementById("ing-hid-choux").setAttribute("hidden", "")
+    document.getElementById("int-itm-choux").removeAttribute("hidden")
+}
+
+function ingAmtChoux(){
+    document.getElementById("ing-itm-choux").setAttribute("hidden", "")
+    document.getElementById("int-amt-choux").removeAttribute("hidden")
+}
+
+function ingHidChoux(){
+    document.getElementById("ing-amt-choux").setAttribute("hidden", "")
+    document.getElementById("int-hid-choux").removeAttribute("hidden")
+}
+
+function showTemp(){
+    document.getElementById("show-temp").removeAttribute("hidden")
+}
+
+function showTime(){
+    document.getElementById("show-time").removeAttribute("hidden")
+}
+
+function proInstructions(){
+    document.getElementById("pro-instr").removeAttribute("hidden")
+}
+
 function hintCreme(){
-    document.getElementById("hint-creme-button").setAttribute("hidden", "")
+    document.getElementById("hid-creme").setAttribute("hidden", "")
     document.getElementById("hint-creme").removeAttribute("hidden")
 }
 
@@ -41,6 +58,26 @@ function detailsCreme(){
 function fullCreme(){
     document.getElementById("details-creme").setAttribute("hidden", "")
     document.getElementById("full-creme").removeAttribute("hidden")
+}
+
+function hideCreme(){
+    document.getElementById("full-creme").setAttribute("hidden", "")
+    document.getElementById("hid-creme").removeAttribute("hidden")
+}
+
+function ingItmCreme(){
+    document.getElementById("ing-hid-creme").setAttribute("hidden", "")
+    document.getElementById("int-itm-creme").removeAttribute("hidden")
+}
+
+function ingAmtCreme(){
+    document.getElementById("ing-itm-creme").setAttribute("hidden", "")
+    document.getElementById("int-amt-creme").removeAttribute("hidden")
+}
+
+function ingHidCreme(){
+    document.getElementById("ing-amt-creme").setAttribute("hidden", "")
+    document.getElementById("int-hid-creme").removeAttribute("hidden")
 }
 
 function hintAssemble(){
