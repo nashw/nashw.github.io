6e912d1e00b40c55950a2c73296f81a746d144c5
nashw <wesleyrnash@gmail.com>
2022-12-10 18:31:34 +0000

    Full recipe

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function moreDesc(){
+    document.getElementById("more-desc").removeAttribute("hidden")
+}
+
 function hintChoux(){
     document.getElementById("hid-choux").setAttribute("hidden", "")
     document.getElementById("hint-choux").removeAttribute("hidden")
@@ -20,17 +24,17 @@ function hideChoux(){
 
 function ingItmChoux(){
     document.getElementById("ing-hid-choux").setAttribute("hidden", "")
-    document.getElementById("int-itm-choux").removeAttribute("hidden")
+    document.getElementById("ing-itm-choux").removeAttribute("hidden")
 }
 
 function ingAmtChoux(){
     document.getElementById("ing-itm-choux").setAttribute("hidden", "")
-    document.getElementById("int-amt-choux").removeAttribute("hidden")
+    document.getElementById("ing-amt-choux").removeAttribute("hidden")
 }
 
 function ingHidChoux(){
     document.getElementById("ing-amt-choux").setAttribute("hidden", "")
-    document.getElementById("int-hid-choux").removeAttribute("hidden")
+    document.getElementById("ing-hid-choux").removeAttribute("hidden")
 }
 
 function showTemp(){
@@ -67,21 +71,60 @@ function hideCreme(){
 
 function ingItmCreme(){
     document.getElementById("ing-hid-creme").setAttribute("hidden", "")
-    document.getElementById("int-itm-creme").removeAttribute("hidden")
+    document.getElementById("ing-itm-creme").removeAttribute("hidden")
 }
 
 function ingAmtCreme(){
     document.getElementById("ing-itm-creme").setAttribute("hidden", "")
-    document.getElementById("int-amt-creme").removeAttribute("hidden")
+    document.getElementById("ing-amt-creme").removeAttribute("hidden")
 }
 
 function ingHidCreme(){
     document.getElementById("ing-amt-creme").setAttribute("hidden", "")
-    document.getElementById("int-hid-creme").removeAttribute("hidden")
+    document.getElementById("ing-hid-creme").removeAttribute("hidden")
+}
+
+function hintChoc(){
+    document.getElementById("hid-choc").setAttribute("hidden", "")
+    document.getElementById("hint-choc").removeAttribute("hidden")
+}
+
+function detailsChoc(){
+    document.getElementById("hint-choc").setAttribute("hidden", "")
+    document.getElementById("details-choc").removeAttribute("hidden")
+}
+
+function fullChoc(){
+    document.getElementById("details-choc").setAttribute("hidden", "")
+    document.getElementById("full-choc").removeAttribute("hidden")
+}
+
+function hideChoc(){
+    document.getElementById("full-choc").setAttribute("hidden", "")
+    document.getElementById("hid-choc").removeAttribute("hidden")
+}
+
+function ingItmChoc(){
+    document.getElementById("ing-hid-choc").setAttribute("hidden", "")
+    document.getElementById("ing-itm-choc").removeAttribute("hidden")
+}
+
+function ingAmtChoc(){
+    document.getElementById("ing-itm-choc").setAttribute("hidden", "")
+    document.getElementById("ing-amt-choc").removeAttribute("hidden")
+}
+
+function ingHidChoc(){
+    document.getElementById("ing-amt-choc").setAttribute("hidden", "")
+    document.getElementById("ing-hid-choc").removeAttribute("hidden")
+}
+
+function coolChoux(){
+    document.getElementById("cool-choux").removeAttribute("hidden")
 }
 
 function hintAssemble(){
-    document.getElementById("hint-assemble-button").setAttribute("hidden", "")
+    document.getElementById("hid-assemble").setAttribute("hidden", "")
     document.getElementById("hint-assemble").removeAttribute("hidden")
 }
 
@@ -93,4 +136,13 @@ function detailsAssemble(){
 function fullAssemble(){
     document.getElementById("details-assemble").setAttribute("hidden", "")
     document.getElementById("full-assemble").removeAttribute("hidden")
+}
+
+function fullAssemble(){
+    document.getElementById("full-assemble").setAttribute("hidden", "")
+    document.getElementById("hid-assemble").removeAttribute("hidden")
+}
+
+function finalStep(){
+    document.getElementById("final-step").removeAttribute("hidden")
 }
